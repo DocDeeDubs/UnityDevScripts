2d1b7b31c82ad689429260e7c547e229b997b01e
DocDeeDubs <57200243+DocDeeDubs@users.noreply.github.com>
2023-09-08 12:57:48 -0700

    Update SpawnGameObjectAtMouseClickLocation.cs
    Scoping and closure of variables. remove of dev output for production version of class

--- SpawnGameObjectAtMouseClickLocation.cs
+++ SpawnGameObjectAtMouseClickLocation.cs
@@ -4,39 +4,30 @@ using UnityEngine;
 
 public class SpawnGameObjectAtMouseClickLocation : MonoBehaviour
 {
+    public SpawnGameObjectAtMouseClickLocation spawnGameObjectAtMouseClickLocation;
     //this class will spawn a game object on any other game object in the scene at the point that is clicked with a mouse
-    
-    //in the inspector, set a prefab as the game object to spawn.
-    public GameObject objectToSpawn;
+    //you may choose to debug log the string results, or feel free to output information about the click or raycast if needed
 
-    //in the inspector, set the camera variable
-    public Camera cam;
-    
-    
-
-    void Update()
+    public string spawnObjectAtClickLocation(GameObject objectToSpawn, Camera mainCamera, Ray ray, RaycastHit hit)
     {
-        if (Input.GetMouseButtonDown(0))
+        string results = "";
+
+        //if it hits a game object within 100 of the camera, spawn the game object prefab at that point.
+        // you can use math.infinity as the range if needed.
+        if (Physics.Raycast(ray, out hit, 100))
         {
-            // cast a ray in from the users screen (the camera) into the 3D scene
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+            //output any results you want here...
+            //example you could debug log the name of the object hit and the point where it was hit at
+            results = "Object spawned onto " + hit.transform.name + " at point " + hit.point.ToString() + " for a distance of " + hit.distance.ToString();
 
-
-            //if it hits a game object within 100 of the camera, spawn the game object prefab at that point.
-            // you can use math.infinity as the range if needed.
-            if (Physics.Raycast(ray, out hit, 100))
-            {
-                //debug log the name of the object hit and the point where it was hit at
-                Debug.Log("I have hit " + hit.transform.name + " at point " + hit.point.ToString());
-
-                //spawn the game object
-                var obj = GameObject.Instantiate(objectToSpawn);
-                //place it in the correct location
-                obj.transform.position = hit.point;
-                //rotate to look at camera
-                obj.transform.LookAt(cam.transform);
-            }
+            //spawn the game object
+            var obj = GameObject.Instantiate(objectToSpawn);
+            //place it in the correct location
+            obj.transform.position = hit.point;
+            //rotate to look at camera
+            obj.transform.LookAt(mainCamera.transform);
         }
+
+        return results;
     }
 }
